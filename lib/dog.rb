47ac40fae1554ac5c827ca024060e852ec6f4804
ziesop20 <sincerelysophie@hotmail.com>
2018-07-27 16:38:30 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,7 @@
 # dog.rb
+class Dog 
+end 
+fido = Dog.new 
+snoopy = Dog.new
+lassie = Dog.new 
+
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,6 @@
 # person.rb
+class Person 
+end 
+
+adele_goldberg = Person.new 
+alan_kay = Person.new
